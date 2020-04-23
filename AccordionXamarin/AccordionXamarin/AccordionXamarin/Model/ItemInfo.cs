0b73c5172d88f0717eab2f4463f23b0947988b44
Jayaleshwari N <jayaleshwarin@syncfusion.com>
2020-04-23 18:08:05 +0530

    XAMARIN-34558 - Sample added.

---
+++ AccordionXamarin/AccordionXamarin/AccordionXamarin/Model/ItemInfo.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using Xamarin.Forms;
+
+namespace AccordionXamarin
+{
+    public class ItemInfo : INotifyPropertyChanged
+    {
+        private string _name;
+        private string _description;
+        private Color _iconColor;
+
+        public Color IconColor
+        {
+            get => _iconColor;
+            set
+            {
+                _iconColor = value;
+                OnPropertyChanged("IconColor");
+            }
+        }
+
+        public string Name
+        {
+            get => _name;
+            set
+            {
+                _name = value;
+                OnPropertyChanged("Name");
+            }
+        }
+        public string Description
+        {
+            get => _description;
+            set
+            {
+                _description = value;
+                OnPropertyChanged("Description");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged(string Property)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(Property));
+        }
+    }
+}
---
+++ AccordionXamarin/AccordionXamarin/AccordionXamarin/ViewModel/ItemInfoRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+using Xamarin.Forms;
+
+namespace AccordionXamarin
+{
+    public class ItemInfoRepository
+    {
+        public ObservableCollection<ItemInfo> Info { get; set; }
+
+        public ItemInfoRepository()
+        {
+            Info = new ObservableCollection<ItemInfo>();
+            Info.Add(new ItemInfo() { IconColor = Color.DarkOrange, Name = "Cheese burger", Description = "Hamburger accompanied with melted cheese. The term itself is a portmanteau of the words cheese and hamburger. The cheese is usually sliced, then added a short time before the hamburger finishes cooking to allow it to melt." });
+            Info.Add(new ItemInfo() { IconColor = Color.DarkBlue, Name = "Veggie burger", Description = "Veggie burger, garden burger, or tofu burger uses a meat analogue, a meat substitute such as tofu, textured vegetable protein, seitan (wheat gluten), Quorn, beans, grains or an assortment of vegetables, which are ground up and formed into patties." });
+            Info.Add(new ItemInfo() { IconColor = Color.DarkRed, Name = "Barbecue burger", Description = "Prepared with ground beef, mixed with onions and barbecue sauce, and then grilled. Once the meat has been turned once, barbecue sauce is spread on top and grilled until the sauce caramelizes." });
+            Info.Add(new ItemInfo() { IconColor = Color.DarkViolet, Name = "Chili burger", Description = "Consists of a hamburger, with the patty topped with chili con carne." });
+        }
+
+    }
+}
